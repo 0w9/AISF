38a88b936d8cc7c8ec65dc4099e4e8df43b8e740
Lennard Dorst <lennardsolana@gmail.com>
2023-06-22 18:42:18 +0200

    Moving the backend to folder

--- blog/.next/static/chunks/webpack.js
+++ blog/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d58461293e9f0101"; }
+/******/ 		__webpack_require__.h = function() { return "c26e3a408b60aac3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
